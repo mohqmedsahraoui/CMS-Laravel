09e568db29540dd8ac79d227c705d7b390b8ec46
Mohamed <mohamedsr99@outlook.fr>
2019-05-20 19:19:28 +0100

    Update app.php
    Co-Authored-By: Bultazo <bultazo@users.noreply.github.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->singleton('filesystem', function ($app) { return $app->loadComponent('filesystems', 'Illuminate\Filesystem\FilesystemServiceProvider', 'filesystem'); });
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
