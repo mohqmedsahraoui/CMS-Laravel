bdeeba470a0ed21964e217f9a40284030ac3550c
Bultazo <33394021+Bultazo@users.noreply.github.com>
2019-05-27 12:48:07 +0000

    Update photo.blade.php

--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -28,14 +28,14 @@
   {{csrf_field()}}
 
         <div class="input-group control-group increment" >
-          <input type="file" name="filename[]" class="form-control">
+          <input type="file" name="source[]" class="form-control">
           <div class="input-group-btn"> 
             <button class="btn btn-success" type="button"><i class="glyphicon glyphicon-plus"></i>Add</button>
           </div>
         </div>
         <div class="clone hide">
           <div class="control-group input-group" style="margin-top:10px">
-            <input type="file" name="filename[]" class="form-control">
+            <input type="file" name="source[]" class="form-control">
             <div class="input-group-btn"> 
               <button class="btn btn-danger" type="button"><i class="glyphicon glyphicon-remove"></i> Remove</button>
             </div>
