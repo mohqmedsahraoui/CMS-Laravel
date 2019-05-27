d8b4f8cc96414166a0e8daf2849396a58199b598
Mohamed <mohamedsr99@outlook.fr>
2019-05-27 13:25:21 +0100

    Create 2019_05_26_124444_add_column_projet_id.php

---
+++ database/migrations/2019_05_26_124444_add_column_projet_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnProjetId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            $table->bigInteger('projet_id')->unsigned();
+            $table->foreign('projet_id')->references('id')->on('projets');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            $table->dropColumn('projet_id');
+        });
+    }
+}
