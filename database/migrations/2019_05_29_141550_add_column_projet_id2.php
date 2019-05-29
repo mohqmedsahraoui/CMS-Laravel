6a08e56fc3e472ed81f33cd443072bf961bd15ed
Bultazo <33394021+Bultazo@users.noreply.github.com>
2019-05-29 14:46:53 +0000

    Create 2019_05_29_141550_add_column_projet_id2.php

---
+++ database/migrations/2019_05_29_141550_add_column_projet_id2.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnProjetId2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            $table->bigInteger('projet_id')->unsigned();
+            $table->foreign('projet_id')->references('id')->on('projets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            $table->dropColumn('projet_id');
+        });
+    }
+}
