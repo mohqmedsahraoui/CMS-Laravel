52d6e643b14cd54d49249efd3e64f8ffee61342d
Bultazo <33394021+Bultazo@users.noreply.github.com>
2019-05-29 14:46:56 +0000

    Create 2019_05_29_140719_create_participants_table.php

---
+++ database/migrations/2019_05_29_140719_create_participants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nom', 200);
+            $table->text('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
