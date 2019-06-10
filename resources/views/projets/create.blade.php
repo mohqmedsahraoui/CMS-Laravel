d0772b897590e2faeeb68307344a200fabe56645
Bultazo <33394021+Bultazo@users.noreply.github.com>
2019-06-10 16:05:23 +0100

    Update create.blade.php

--- resources/views/projets/create.blade.php
+++ resources/views/projets/create.blade.php
@@ -77,41 +77,13 @@
                 
                 <div class="form-group">
 
-                <label for="">Nom</label>
-            <input type="text" class="form-control">
-                    
+          
             </div>
 
                     
-           
-            <div class="form-group">
-        <select id="list" class="form-control"  onchange="getSelectValue();">
-        <option value="value">Choisir le rôle</option>
-            <option value="Maitre A">Maitre A</option>
-            <option value="Maitre B">Maitre B</option>
-            <option value="Maitre C">Maitre C</option>
-            
-        </select>
-    <script>
         
-        function getSelectValue()
-        {
-            var selectedValue = document.getElementById("list").value;
-            console.log(selectedValue);
-        }
-        getSelectValue();
-
-    </script>
 
 
-
-                   
-    
-
-                     
-</div>
-
-</div>
 
  <input type="submit" name="présentation" class="form-control btn btn-primary" value=Suivant>
             </div>
