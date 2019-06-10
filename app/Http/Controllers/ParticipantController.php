922f5c5358f429a7dd68220bb40ac287601b79be
Bultazo <33394021+Bultazo@users.noreply.github.com>
2019-06-10 13:11:23 +0100

    Update ParticipantController.php

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\cmsRequest;
+use App\Participant;
 use Illuminate\Http\Request;
 
 class ParticipantController extends Controller
+
 {
-    //
+
+    public function create() {
+
+        return view("projets.participant");
+
+
+    }
+
+    public function store (cmsRequest $request) {
+    $participant = new participant ();
+    
+    $participant->role = $request -> input('participant');
+
+   
+
+    }
 }
