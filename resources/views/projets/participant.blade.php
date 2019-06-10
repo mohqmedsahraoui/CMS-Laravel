3c16fcc3a7be3147c4630fb21ffc09e2fde0a223
Bultazo <33394021+Bultazo@users.noreply.github.com>
2019-06-10 16:05:27 +0100

    Update participant.blade.php

--- resources/views/projets/participant.blade.php
+++ resources/views/projets/participant.blade.php
@@ -3,27 +3,48 @@
 @section('content')
 
 <body>
+
+
+<form method="post" action="{{url('participant')}}" enctype="multipart/form-data">
+
     
 <div>
          
                 
-         <div class="form-group">
+
+    <div class="form-group">
 
          <label for="">Nom</label>
-     <input type="text" class="form-control">
+     <input type="text" name ="nom" class="form-control">
              
      </div>
 
              
     
-     <div class="form-group">
- <select id="list" class="form-control"  onchange="getSelectValue();">
- <option value="value">Choisir le rôle</option>
-     <option value="Maitre A">Maitre A</option>
-     <option value="Maitre B">Maitre B</option>
-     <option value="Maitre C">Maitre C</option>
+    <div class="form-group">
+        <select id="list" class="form-control"  onchange="getSelectValue();">
+            <option value="value">Choisir le rôle</option>
+            <option value="Maitre A">Maitre A</option>
+            <option value="Maitre B">Maitre B</option>
+            <option value="Maitre C">Maitre C</option>
      
- </select>
+        </select>
+
+    </div>
+
+
+
+</div>
+<input type="number" hidden="hidden" value = "{{$fares2}}" name="projet_id2" >
+
+<button type="submit" class="btn btn-primary" style="margin-top:10px">Submit</button>
+
+</form>
+
+
+
+
+
 <script>
  
  function getSelectValue()
@@ -34,10 +55,6 @@
  getSelectValue();
 
 </script>
-
-</div>
-
-</div>
 
 </body>
     
