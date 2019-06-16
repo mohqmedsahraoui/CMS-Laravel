e34817390019e1b0af8d0a305c3b5c528ff51873
Bultazo <33394021+Bultazo@users.noreply.github.com>
2019-06-16 20:00:26 +0100

    Update ParticipantController.php

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -1,31 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Requests\cmsRequest;
-use App\Participant;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Participant;
 
 class ParticipantController extends Controller
-
 {
-
-    public function create(){
-
-        return view("projets.participant");
-
-
+    public function create()
+    {
+        $data3 = DB::table('projets')->whereRaw('id')->get('id')->last();
+        $data4 = implode("/n", array_flatten($data3));
+        return view('projets.participant', ['data4' => $data4]);
     }
 
-    public function store(cmsRequest $request) {
-        return "saved"; 
+    public function store(Request $request)
+    {
+        $participant = new Participant();
 
-    $participant = new participant();
-    $participant->nom = $request->input('nom');
-    $participant->role = $request->input('role');
-    $participant->projet_id = $request->input('projet_id');
-    $participant->save();
-
-    
-    
+        $participant->nom = $request->input('nom');
+        $participant->role = $request->input('role');
+        $participant->projet_id = $request->input('projet_id');
+        $participant->save();
+        
+        return redirect('projets');
     }
 }
